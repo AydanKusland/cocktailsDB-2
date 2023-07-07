80f591807e4210a44895bb1e75e780a767d8f922
AydanKusland <chinatugov@mail.ru>
2023-07-07 19:26:21 +0800

    first

---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import Landing from './Landing'
+import About from './About'
+import Cocktail from './Cocktail'
+import Error from './Error'
+import Newsletter from './Newsletter'
+import HomeLayout from './HomeLayout'
+
+export { Landing, About, Cocktail, Error, Newsletter, HomeLayout }
+export { default as SinglePageError } from './SinglePageError'
